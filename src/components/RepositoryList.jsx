d88b7cc24f3c40a48730c6c5082b2db88eaa2a6e
Kleber Rodrigo de Carvalho <kleber.carvalho@ibm.com>
2021-04-18 01:51:56 -0300

    first react component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,16 @@
+export function RepositoryList() {
+  return (
+    <section className="respository-list">
+      <h1>Lista de repositório</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms is React</p>
+
+          <a href="">Acessar repositórios</a>
+
+        </li>
+      </ul>
+    </section>
+  );
+}
