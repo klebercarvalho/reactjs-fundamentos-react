a45feb034d066dff39fd9103a2edcb2e99cde218
Kleber Rodrigo de Carvalho <kleber.carvalho@ibm.com>
2021-04-26 23:56:43 -0300

    listing repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,12 +4,6 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
 
 
-const repository = {
-  name: 'unform',
-  description: 'Form in React',
-  link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -25,10 +19,10 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de repositório</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
+
       </ul>
     </section>
   );
